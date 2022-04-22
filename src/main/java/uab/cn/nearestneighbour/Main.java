3aa5235a91c407991ca0fb65c6ffeac8fa828fbd
João Nobre <joaoanobre@gmail.com>
2022-04-22 18:11:50 +0200

    Add Nearest Neighbour algorithm

--- src/main/java/uab/cn/nearestneighbour/Main.java
+++ src/main/java/uab/cn/nearestneighbour/Main.java
@@ -2,7 +2,45 @@ package uab.cn.nearestneighbour;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static int[][] readInput(String fileString) {
+        int[][] distanceMatrix = new int[10000 + 1][10000 + 1];
+        for (int[] row : distanceMatrix) java.util.Arrays.fill(row, 10000);
+        distanceMatrix[0][0] = 1;
+        for (int i = 1; i <= 10000; i++)
+        {
+            for (int j = 1; j <= 10000; j++)
+            {
+                if (i == j)
+                {
+                    distanceMatrix[i][j] = 1;
+                }
+            }
+        }
+/*
+        for (int i = 0 ; i < distanceMatrix.length ; i++) {
+            for (int j = 0 ; j < distanceMatrix[i].length ; j++) {
+                System.out.print(distanceMatrix[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+*/
+        return distanceMatrix;
+    }
 
+    public static void main(String[] args) {
+        int adjCitiesDistance[][];
+
+        System.out.println("*** Problema 1 - Capitais de distrito Portuguesas ***");
+        adjCitiesDistance = readInput("teste");
+        long start2 = System.currentTimeMillis();
+        NearestNeighbour nearestNeighbour = new NearestNeighbour();
+        nearestNeighbour.tsp(adjCitiesDistance);
+        long end2 = System.currentTimeMillis();
+        System.out.println("\n[Problem 1] Elapsed Time in milli seconds: "+ (end2-start2));
+        System.out.println("\n*****************************************************");
+
+        System.out.println("*** Problema 2 - Capitais Europeias ***");
+        System.out.println("TODO");
+        System.out.println("\n****************************************");
     }
 }
--- src/main/java/uab/cn/nearestneighbour/NearestNeighbour.java
+++ src/main/java/uab/cn/nearestneighbour/NearestNeighbour.java
@@ -11,5 +11,38 @@ public class NearestNeighbour {
         stack = new Stack<Integer>();
     }
 
+    public void tsp(int adjCitiesDistance[][]) {
+        nNodes = adjCitiesDistance[1].length - 1;
+        int[] visited = new int[nNodes + 1];
+        visited[1] = 1;
+        stack.push(1);
+        int element, dst = 0 ,i;
+        int min = Integer.MAX_VALUE;
+        boolean minFlag = false;
+        System.out.print(1 + "\t");
 
+        while (!stack.isEmpty()) {
+            element = stack.peek();
+            i = 1;
+            min = Integer.MAX_VALUE;
+            while (i <= nNodes) {
+                if (adjCitiesDistance[element][i] > 1 && visited[i] == 0) {
+                    if (min > adjCitiesDistance[element][i]) {
+                            min = adjCitiesDistance[element][i];
+                            dst = i;
+                            minFlag = true;
+                    }
+                }
+                i++;
+            }
+            if (minFlag) {
+                visited[dst] = 1;
+                stack.push(dst);
+                System.out.print(dst + "\t");
+                minFlag = false;
+                continue;
+            }
+            stack.pop();
+        }
+    }
 }
